e698998374a774f1d2bd9004f5e7602efa924d72
Ankur Gupta <rukna1000@gmail.com>
2020-11-10 14:53:54 -0500

    Trying to get jupytext to work

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 
 setup(name='auto_trainer',
       version='0.0.1',
-      install_requires=['gym_solo', 'tensorflow-gpu', 'stable-baselines[mpi]'] 
+      install_requires=['stable-baselines[mpi]', 'tensorflow-gpu'] 
 )
