bbd1efb0e74cd4a7e00444b6c402dd514046fece
Ankur Gupta <rukna1000@gmail.com>
2020-12-14 16:36:37 -0500

    Add postiive wandb test

--- auto_trainer/test_trainer.py
+++ auto_trainer/test_trainer.py
@@ -3,6 +3,8 @@ import unittest
 from auto_trainer import trainer
 from unittest import mock
 
+from wandb.integration.sagemaker import config
+
 
 class TestAutoTrainer(unittest.TestCase):
   def test_synced_config_no_wandb(self):
@@ -15,6 +17,22 @@ class TestAutoTrainer(unittest.TestCase):
     
     self.assertDictEqual(config, param)
     self.assertIsNone(run)
+
+  @mock.patch('wandb.init')
+  def test_synced_config_wandb(self, mock_wandb):
+    param = {'test_key': 'test_value'}
+    tags = ['bunch', 'of', 'tags']
+
+    mock_run = mock.Mock(config=param)
+    mock_wandb.return_value = mock_run
+
+    trainer._WANDB = True
+    self.assertTrue(trainer._WANDB)
+    
+    config, run = trainer.get_synced_config(param, tags)
+
+    self.assertDictEqual(config, param)
+    self.assertEqual(run, mock_run)
 
 
 if __name__ == '__main__':
